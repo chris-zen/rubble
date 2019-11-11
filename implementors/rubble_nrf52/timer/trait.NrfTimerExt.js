786a896657e7e903bdf57e4ecd14e33d9d5c6521
Travis CI User <travis@example.org>
2019-11-11 00:21:28 +0000

    Update documentation

---
+++ implementors/rubble_nrf52/timer/trait.NrfTimerExt.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["rubble_nrf52"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
